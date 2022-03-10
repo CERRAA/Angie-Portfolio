a0aa74119a781acd9854dbaefe11603e0e63f346
CERRAA <cerraangie113@gmail.com>
2022-03-10 11:02:39 +0300

    my image

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+  
+  const textarea = document.querySelector("textarea");
+  textarea.addEventListener("keyup", e =>{
+    let scHeight = e.target.scrollHeight;
+    textarea.style.height = `${scHeight}px`;
+  })
